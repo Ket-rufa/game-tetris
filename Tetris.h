0971a459d74d9fdc841ec607457fc61010bc9027
Ket-rufa <nket865@gmail.com>
2023-12-26 00:55:33 +0700

    files

---
+++ Tetris.h
@@ -0,0 +1,38 @@
+
+// Tetris.h : main header file for the Tetris application
+//
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"       // main symbols
+
+
+// CTetrisApp:
+// See Tetris.cpp for the implementation of this class
+//
+
+class CTetrisApp : public CWinAppEx
+{
+public:
+	CTetrisApp() noexcept;
+
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+
+// Implementation
+	BOOL  m_bHiColorIcons;
+
+	virtual void PreLoadState();
+	virtual void LoadCustomState();
+	virtual void SaveCustomState();
+
+	afx_msg void OnAppAbout();
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CTetrisApp theApp;
